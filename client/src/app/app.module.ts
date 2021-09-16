695d263745c50b52c70f3f7e1811e80ae75820c6
Alec Gillis <gillismail1213@gmail.com>
2021-09-15 23:14:50 -0400

    Add liked-songs component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LikedSongsComponent } from './liked-songs/liked-songs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikedSongsComponent
   ],
   imports: [
     BrowserModule,
