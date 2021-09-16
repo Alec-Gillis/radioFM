3f9ce988865ab0596c92138bcd6e3e967a757586
Alec Gillis <gillismail1213@gmail.com>
2021-09-15 23:52:02 -0400

    Add routing to liked-songs component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LikedSongsComponent } from './liked-songs/liked-songs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'liked-songs-component', component: LikedSongsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
